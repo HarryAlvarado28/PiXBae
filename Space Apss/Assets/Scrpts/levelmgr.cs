a525624789119c30f90df615ccc484fd4a20dc0f
Moisès Chiari <moy@MacBook-Pro-de-Moises.local>
2018-10-21 05:28:38 -0500

    Upload by moiseschiari

--- Space Apss/Assets/Scrpts/levelmgr.cs
+++ Space Apss/Assets/Scrpts/levelmgr.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class levelmgr : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+   
+    // Use this for initialization
+    void Start () {
+      
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,6 +17,13 @@ public class levelmgr : MonoBehaviour {
     public void nextLevel(string level)
     {
         SceneManager.LoadScene(level);
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Se ha salido del juego");
 
     }
 }
