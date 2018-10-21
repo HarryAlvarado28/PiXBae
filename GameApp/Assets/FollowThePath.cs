0709b267386d4eb8219cb42afc26890a74490667
unknown <andres.jimeneza@up.ac.pa>
2018-10-21 14:44:40 -0500

    Beta

---
+++ GameApp/Assets/FollowThePath.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class FollowThePath : MonoBehaviour {
+
+    // Number of waypoints in the board
+    public Transform[] waypoints;
+
+    [SerializeField]
+    private float moveSpeed = 1f; // Speed of the spaceship
+
+    [HideInInspector]
+    public int waypointIndex = 0; // Index of the waypoint array
+
+    public bool moveAllowed = false;
+
+	// Use this for initialization
+	private void Start () {
+        transform.position = waypoints[waypointIndex].transform.position;
+	}
+	
+	// Update is called once per frame to check if the player can move
+	private void Update () {
+        if (moveAllowed)
+            Move();
+	}
+
+
+    // This is to move the player to the indexed waypoint
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,
+            waypoints[waypointIndex].transform.position,
+            moveSpeed * Time.deltaTime);
+
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+        }
+    }
+}
---
+++ GameApp/Assets/GameControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+
+    private static GameObject player1;
+
+    public static int diceSideThrown = 0;
+    public static int player1StartWaypoint = 0;
+
+    public static bool gameOver = false;
+
+    // Use this for initialization
+    void Start () {
+
+        
+   
+       
+        // Find the spaceship from the player 1
+        player1 = GameObject.Find("Player1");
+
+        player1.GetComponent<FollowThePath>().moveAllowed = false;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This is to hold the spaceship when it reaches the end
+        if (player1.GetComponent<FollowThePath>().waypointIndex > 
+            player1StartWaypoint + diceSideThrown)
+        {
+            player1.GetComponent<FollowThePath>().moveAllowed = false;
+            player1StartWaypoint = player1.GetComponent<FollowThePath>().waypointIndex - 1;
+        }
+
+
+
+
+    }
+
+    // It allows the player to move
+    public static void MovePlayer(int playerToMove)
+    {
+        switch (playerToMove) { 
+            case 1:
+                player1.GetComponent<FollowThePath>().moveAllowed = true;
+                break;
+        }
+    }
+}
